73840fa4db2df17f40cb1a25cdc9c17701cb6bb0
SamarthBagde <bagdesamarth70@gmail.com>
2025-06-20 18:20:49 +0530

    added dockerfile and docker-compose file

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../style/login.css";
+import "../style/Login.css";
 import ErrorMsg from "../components/ErrorMsg";
 
 const Login = () => {
--- Frontend/src/pages/UploadWork.jsx
+++ Frontend/src/pages/UploadWork.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import styles from "../style/uploadWork.module.css";
+import styles from "../style/UploadWork.module.css";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
